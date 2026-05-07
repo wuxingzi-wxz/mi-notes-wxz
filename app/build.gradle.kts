15a5d748a05e00773e3b8b382c1a27b03ff5bde9
wuxingzi-wxz <wuxingzi-wxz@users.noreply.github.com>
2026-05-07 22:33:37 +0800

    GTaskASyncTask.java中修改废弃的 setLatestEventInfo 方法 build.gradle.kts中添加useLibrary("org.apache.http.legacy") 修改所有switch警告 AndroidManifest.xml中添加provider

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "net.micode.notes"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "net.micode.notes"
+        minSdk = 36
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    useLibrary("org.apache.http.legacy")
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
